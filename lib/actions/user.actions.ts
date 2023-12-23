d9a4df6e689b47aeb366ece8bf41fd3c713936ec
saerelmasri <saer1890@gmail.com>
2023-12-23 14:37:10 +0200

    Fixed the use server code

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,4 +1,4 @@
-"user server";
+"use server";
 
 import { CreateUserParams, UpdateUserParams } from "@/types";
 import { handleError } from "../utils";
@@ -19,56 +19,58 @@ export const createUser = async (user: CreateUserParams) => {
 };
 
 export const updateUser = async (clerkId: string, user: UpdateUserParams) => {
-    try {
-        await connectToDatabase();
-        const updatedUser = await User.findOneAndUpdate({clerkId}, user, { new: true});
-        if(!updateUser){
-            throw new Error("User update failed");
-        }
-        return JSON.parse(JSON.stringify(updateUser));
-    } catch (error) {
-        handleError(error);
+  try {
+    await connectToDatabase();
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
+    if (!updateUser) {
+      throw new Error("User update failed");
     }
+    return JSON.parse(JSON.stringify(updateUser));
+  } catch (error) {
+    handleError(error);
+  }
 };
 
 export const deletedUser = async (clerkId: string) => {
-    try {
-        await connectToDatabase();
-        const userToDelete = await User.findOne({ clerkId });
+  try {
+    await connectToDatabase();
+    const userToDelete = await User.findOne({ clerkId });
 
-        if(!userToDelete){
-            throw new Error("User not found");
-        }
-
-        await Promise.all([
-            Event.updateMany(
-                {_id: { $in: userToDelete.events }},
-                {$pull: { organizer: userToDelete._id }}
-            ),
-            Order.updateMany(
-                {_id: {$in: userToDelete.orders }},
-                {$unset: {buyer: 1}}
-            )
-        ]);
-
-        const deletedUser = await User.findByIdAndDelete(userToDelete._id);
-        revalidatePath('/');
-
-        return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
-    } catch (error) {
-        handleError(error);
+    if (!userToDelete) {
+      throw new Error("User not found");
     }
-}
+
+    await Promise.all([
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organizer: userToDelete._id } }
+      ),
+      Order.updateMany(
+        { _id: { $in: userToDelete.orders } },
+        { $unset: { buyer: 1 } }
+      ),
+    ]);
+
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+  } catch (error) {
+    handleError(error);
+  }
+};
 
 export const getUserById = async (userId: string) => {
-    try {
-        await connectToDatabase();
-        const user = await User.findById(userId);
-        if(!user){
-            throw new Error("User not found");
-        }
-        return JSON.parse(JSON.stringify(user));
-    } catch (error) {
-        handleError(error);
+  try {
+    await connectToDatabase();
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error("User not found");
     }
-}
+    return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
+  }
+};
