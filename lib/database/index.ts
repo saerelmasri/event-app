4b11043ea436e80bfb3475791152d35485af6d50
saerelmasri <saer1890@gmail.com>
2023-12-23 16:27:26 +0200

    Chanfe db config

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,29 +5,18 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  try {
-    if (cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-    if (!MONGODB_URI) {
-      throw new Error("MONGODB_URI is missing");
-    }
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-    console.log("Connecting to MongoDB...");
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "evently",
+      bufferCommands: false,
+    });
 
-    cached.promise =
-      cached.promise ||
-      mongoose.connect(MONGODB_URI, {
-        dbName: "eventdb",
-        bufferCommands: false,
-      });
+  cached.conn = await cached.promise;
 
-    cached.conn = await cached.promise;
-
-    console.log("Connected to MongoDB!");
-
-    return cached.conn;
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    throw error; // Re-throw the error to let the calling code handle it
-  }
+  return cached.conn;
 };
