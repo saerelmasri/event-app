d39a1144c09157c7adba01841716599e2293b61c
saerelmasri <saer1890@gmail.com>
2023-12-23 15:54:53 +0200

    Changed db name

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -17,7 +17,7 @@ export const connectToDatabase = async () => {
     cached.promise =
       cached.promise ||
       mongoose.connect(MONGODB_URI, {
-        dbName: "event-app",
+        dbName: "eventDB",
         bufferCommands: false,
       });
 
