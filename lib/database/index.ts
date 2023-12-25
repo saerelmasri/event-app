60dfe5ac1896f75d8adab3306af0ed5eea0e3e0b
saerelmasri <saer1890@gmail.com>
2023-12-25 19:14:57 +0200

    .

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,21 +2,30 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is missing in environment variables");
+}
+
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+  try {
+    cached.promise =
+      cached.promise ||
+      mongoose.connect(MONGODB_URI, {
+        dbName: "evently",
+        bufferCommands: false,
+      });
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URI, {
-      dbName: "evently",
-      bufferCommands: false,
-    });
+    cached.conn = await cached.promise;
 
-  cached.conn = await cached.promise;
-
-  return cached.conn;
+    return cached.conn;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw new Error("Error connecting to MongoDB");
+  }
 };
